9bbb1d0e44cd028e641b0177e9d771b4cd555ec8
MichelSchade <57805620+MichelSchade@users.noreply.github.com>
2020-12-09 16:21:54 +0100

    Create klaro.js

---
+++ klaro.js
@@ -0,0 +1,116 @@
+var klaroConfig = {
+  elementID: 'klaro',
+  storageMethod: 'cookie',
+  cookieName: 'Parkour Creation e.V. Cookies',
+  cookieExpiresAfterDays: 30,
+  privacyPolicy: 'https://www.diehalle.hamburg/impressum',
+  default: false,
+  mustConsent: true,
+  acceptAll: true,
+  hideDeclineAll: true,
+  lang: 'de',
+  translations: {
+    de: {
+      consentModal: {
+        description:
+                    'Hier kannst du einsehen und anpassen, welche Information wir über dich sammeln.',
+      },
+      memberstack: {
+        description: 'Mitgliederdaten',
+      },
+      ganalytics: {
+        description: 'Echtzeit-Benutzeranalyse',
+      },
+      tagmanager: {
+        description: 'Echtzeit-Benutzeranalyse',
+      },
+      hotjar: {
+        description: 'Echtzeit-Benutzeranalyse',
+      },
+      fpixel: {
+        description: 'Echtzeit-Benutzeranalyse',
+      },
+      purposes: {
+        analytics: 'Besucher-Statistiken',
+        security: 'Sicherheit',
+        styling: 'Styling',
+      },
+    },   
+  },
+  apps: [
+    {
+      name: 'hotjar',
+      default: false,
+      title: 'Hotjar',
+      purposes: ['analytics'],
+      cookies: ['external-tracker'],
+      callback: function(consent, app) {
+        console.log(
+          'User consent for app ' + app.name + ': consent=' + consent
+        );
+      },
+      required: false,
+      optOut: false,
+      onlyOnce: true,
+    },
+    {
+      name: 'ganalytics',
+      title: 'Google Analytics',
+      purposes: ['analytics'],
+      cookies: ['external-tracker'],
+      callback: function(consent, app) {
+        console.log(
+          'User consent for app ' + app.name + ': consent=' + consent
+        );
+      },
+      optOut: false,
+    },
+    {
+      name: 'fpixel',
+      title: 'Facebook Pixel',
+      purposes: ['analytics'],
+      cookies: ['external-tracker'],
+      callback: function(consent, app) {
+        if (consent == true ) { 
+          fbq('consent', 'grant');
+          fbq('track', 'PageView');
+          window.localStorage.setItem('fbGrantConsent', 'true');
+        } else {
+          fbq('consent', 'revoke');
+          window.localStorage.setItem('fbGrantConsent', 'false');
+        };
+        console.log(
+          'User consent for app ' + app.name + ': consent=' + consent
+        );
+      },
+      optOut: false,
+    },
+    {
+      name: 'memberstack',
+      title: 'Memberstack',
+      purposes: ['security'],
+      cookies: ['external-tracker'],
+      callback: function(consent, app) {
+        console.log(
+          'User consent for app ' + app.name + ': consent=' + consent
+        );
+      },
+      optOut: false,
+    },
+    {
+      name: 'tagmanager',
+      title: 'Google Tag Manager',
+      purposes: ['analytics'],
+      cookies: ['external-tracker'],
+      callback: function(consent, app) {
+        if (consent !== false) {
+          dataLayer.push({'event' : 'loadgtm-analytics'});
+        }
+        console.log(
+          'User consent for app ' + app.name + ': consent=' + consent
+        );
+      },
+      optOut: false,
+    },
+  ],
+};
