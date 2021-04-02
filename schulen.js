8b9c6be93e051c3dcf27af97e60230b39c8de238
MichelSchade <57805620+MichelSchade@users.noreply.github.com>
2021-04-02 11:30:46 +0200

    Update schulen.js

--- schulen.js
+++ schulen.js
@@ -145,7 +145,9 @@ function on_planyo_form_loaded(event) {
     } else if (jQuery('#rental_prop_children option:selected').val() > 0) {
       jQuery('#rental_prop_Teilnehmerdaten_1').removeClass('hidefromcustomer');
     }
-  } else if (event == 'reservation_details' || event == 'payment_form' || event == 'reservation_done' ||  event == 'checkout') {
+  }
+
+  if (event == 'reservation_details' || event == 'payment_form' || event == 'reservation_done' ||  event == 'checkout') {
     jQuery('html, body').animate({
       scrollTop: jQuery("#planyo_content").offset().top
     }, 500);
@@ -153,7 +155,20 @@ function on_planyo_form_loaded(event) {
     jQuery('.reservation_step_1, .reservation_step_2, .reservation_step_3').addClass('completed');
     jQuery('.completed .reservation_step_img_text').html('<img src="https://uploads-ssl.webflow.com/5e5bcb9d0e170635e182079a/5e9ff8e6f9a5b1d89d40f5cb_check-solid.svg" width=50% />');
   }
+  
   if (event == 'reservation_failure') {
     jQuery('#price_info_div_sticky, #res_form_buttons').css('display','block !important');
+  }
+  
+  if (event == 'reservation_done') {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      'event': 'gtm_conversion',
+      'conversionInfo': {
+        'productName': jQuery('#rs').text(),
+        'productId': jQuery('#pid').text(),
+        'productPrice': jQuery('#preis').text().split(" ")[0]
+      }
+    });
   }
 }
