64add2b01e267496738debe144e53040d086b77a
MichelSchade <57805620+MichelSchade@users.noreply.github.com>
2021-04-02 12:56:21 +0200

    Update personal.js

--- personal.js
+++ personal.js
@@ -139,7 +139,7 @@ function on_planyo_form_loaded(event) {
   }
   
   if (event == 'reservation_failure') {
-    jQuery('#price_info_div_sticky, #res_form_buttons').css('display','block !important');
+    jQuery('#price_info_div_sticky, #res_form_buttons, #multipage_prev').show();
   }
 }
 jQuery(document).ready(function() {
